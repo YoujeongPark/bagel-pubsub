b61df54450ef50f31964f7af4bb4f7ab80c6df83
Youjeong Park <pyj0695@gmail.com>
2024-01-01 10:17:23 +0900

    feat : add Bean (asyncConfig)

---
+++ bagel-main/src/main/java/com/bagel/main/websocket/config/AsyncConfig.java
@@ -0,0 +1,21 @@
+package com.bagel.main.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+    @Bean
+    public AsyncTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.initialize();
+        return executor;
+    }
+}
